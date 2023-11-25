0a47e49221fdfa81547dbe805ca957168aeb7593
ajmalkhan <ajmalkhan92@gmail.com>
2023-11-25 14:15:00 +0530

    Added files

--- src/test/E2E/PlaceOrderTests.java
+++ src/test/E2E/PlaceOrderTests.java
@@ -1,2 +1,30 @@
-package src.test.E2E;public class PlaceOrderTests {
+package src.test.E2E;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.*;
+import org.openqa.selenium.chrome.*;
+
+public class PlaceOrderTests {
+    private WebDriver driver;
+
+    @BeforeSuite
+    public void initializeDriver() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\MY-PC\\Documents\\EcommerceAutomation\\src\\main\\resources\\driver\\chromedriver.exe");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        driver.get("https://automationexercise.com/");
+    }
+
+    @Test
+    public void SampleTest() {
+        System.out.println("Test method");
+    }
+
+    @AfterSuite
+    public void closeDriver() {
+        driver.quit();
+    }
 }
